04c4e650980c33ae64adcf75cd8d1a711bd9fcd5
Mário Feroldi <mferoldif@gmail.com>
2022-07-05 13:14:35 -0300

    feat: try to install pyenv through packager manager

--- tools/pyenv/install-on-linux.sh
+++ tools/pyenv/install-on-linux.sh
@@ -2,13 +2,23 @@
 
 source ../installation-util.sh
 
-ensure_has_commands git make
+has_command pyenv && {
+    say 'pyenv is already installed'
+    exit 0
+}
 
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-(cd ~/.pyenv && src/configure && make -C src)
+# FIXME(feroldi): I'm not sure which path the package gets installed
+# into. We need to know that in order to set PYENV_ROOT.
+if ! install_package pyenv; then
+    ensure_has_commands git make
 
-printf '%s\n' 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
-printf '%s\n' 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    (cd ~/.pyenv && src/configure && make -C src)
+
+    printf '%s\n' 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+    printf '%s\n' 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+fi
+
 printf '%s\n' 'eval "$(pyenv init -)"' >> ~/.profile
 
 say 'you need to logout in order to make the new changes work'
