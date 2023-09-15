01194dc1b82ec1b640b68ce6fb86079489f3f647
Mario Feroldi <mferoldif@gmail.com>
2023-09-15 00:04:58 -0300

    fix: install packer before syncing

--- tools/nvim/install-on-linux.sh
+++ tools/nvim/install-on-linux.sh
@@ -8,4 +8,6 @@ mkdir -p $HOME/.config/nvim
 
 cp -r ./settings/* $HOME/.config/nvim/
 
+git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 nvim -E -c PackerSync -c q
