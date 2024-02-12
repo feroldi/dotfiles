4498b6ee9c1ade44e799564dc5353d1accf6828c
Mário Feroldi <mferoldif@gmail.com>
2024-02-12 20:50:27 -0300

    add bash tool

--- tools/bash/install-on-linux.sh
+++ tools/bash/install-on-linux.sh
@@ -2,7 +2,8 @@
 
 source ../installation-util.sh
 
-install_package bash
+install_package bash zoxide
 
 cp bashrc $HOME/.bashrc
 cp bash_aliases $HOME/.bash_aliases
+cp bash_profile $HOME/.bash_profile
