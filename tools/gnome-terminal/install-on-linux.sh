23ab6af470480869db169bd4391eac77403f2e8c
Mário Feroldi <mferoldif@gmail.com>
2026-05-29 15:59:15 -0300

    add gnome terminal

--- tools/gnome-terminal/install-on-linux.sh
+++ tools/gnome-terminal/install-on-linux.sh
@@ -4,4 +4,4 @@ source ../installation-util.sh
 
 install_package gnome-terminal
 
-dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9 < ./gnome_term_all_settings.txt
+dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < ./gnome_term_all_settings.txt
